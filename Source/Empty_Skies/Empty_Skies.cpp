889b1eb5338a510a4e93d3d4b9cf53f88dadf297
DillonMarchese <dillon.marchese@gmail.com>
2015-03-24 15:13:06 -0500

    Initial Commit
    Updated to UE4.7
    
    Make sure you've updated to this version before opening the project.(its
    way better anyway)

---
+++ Source/Empty_Skies/Empty_Skies.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Empty_Skies.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Empty_Skies, "Empty_Skies" );
