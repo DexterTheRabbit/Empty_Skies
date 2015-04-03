4425499225298d1c5e4d458d0ec0996f4c6bd6f0
DillonMarchese <dillon.marchese@gmail.com>
2015-04-03 12:17:51 -0500

    Dillon 2
    Temporary "working" version. Still a bug in deathmatch gamemode causing
    strange things. May have to try moving code to the level blueprint for
    ease of use

---
+++ Source/Empty_Skies/MyClass.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+/**
+ * 
+ */
+class EMPTY_SKIES_API MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+};
